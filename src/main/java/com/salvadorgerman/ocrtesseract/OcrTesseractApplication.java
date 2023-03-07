e132aaae30e0b7ddf63dccda83098781bac8b1a1
Macailh <salvadorenriquegb@gmail.com>
2023-03-06 19:15:07 -0600

    Initial commit

---
+++ src/main/java/com/salvadorgerman/ocrtesseract/OcrTesseractApplication.java
@@ -0,0 +1,13 @@
+package com.salvadorgerman.ocrtesseract;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OcrTesseractApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OcrTesseractApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/salvadorgerman/ocrtesseract/OcrTesseractApplicationTests.java
@@ -0,0 +1,13 @@
+package com.salvadorgerman.ocrtesseract;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OcrTesseractApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
